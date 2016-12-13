36adc71844379d86ef7b163d73bb7dff9d358e12
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-13 22:30:45 +0000

    starting out

---
+++ my_shell/holberton.h
@@ -0,0 +1,11 @@
+#ifndef MY_SHELL
+#define MY_SHELL
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handle_signal(int signo);
+
+
+#endif /* ifndef MY_SHELL */
